2d5df9288b46238451f4493f764c8f7437d2bb99
omi1811 <omkarshrotri2021.ainds@mmcoe.edu.in>
2026-05-21 15:57:16 +0530

    Done with Users, Posts and Comments

--- src/models.py
+++ src/models.py
@@ -24,3 +24,16 @@ class Post(Base):
     author_id = Column(Integer, ForeignKey('users.id'))
     
     author = relationship('User', back_populates='posts')
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    
+    id = Column(Integer, primary_key=True, index = True)
+    body = Column(Text, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    author_id = Column(Integer, ForeignKey('users.id'))
+    
+    post = relationship('Post', back_populates='comments')
+    author = relationship('User', back_populates='comments')
+
